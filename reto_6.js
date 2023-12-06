84d9d136c5327a4ab5dce5c4c91a1bd4080bfeb1
Oscar Gandia Ubeda <ogandia@ontinet.com>
2023-12-06 15:15:20 +0100

    realizacion reto 6

---
+++ reto_6.js
@@ -0,0 +1,24 @@
+console.log(maxDistance('>>>*<<<<'))
+
+function maxDistance(movements) {
+  let distance = 0
+  let plusDir = 0
+  let minusDir = 0 
+
+  for (const char of movements) {
+    if (char === '>') plusDir++
+    else if (char === '<') minusDir++
+  }
+
+  for (const char of movements) {
+    if (char === '*') {
+      if (plusDir > minusDir) distance++
+      else if (plusDir < minusDir) distance--
+      else distance++
+    } else {
+      distance = char === '>' ? distance + 1 : distance - 1
+    }
+  }
+
+  return Math.abs(distance);
+}
